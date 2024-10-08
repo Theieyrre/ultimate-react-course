bff728760bd0607863e3ed3f12982c9349931534
Mustafa Ünal <crizfmtfm@gmail.com>
2024-10-08 10:10:07 +0300

    End of challenge 3

---
+++ 0X-challenges/challenge3-date-counter/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div className="row">
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p className="message">
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
+
+export default App;
